6051f32c00b0c71d62b071a356b5eadb42026fdf
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-06 13:39:48 -0700

    Working birthday output with conversions

--- Dates/main.m
+++ Dates/main.m
@@ -10,8 +10,12 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        NSNumber *age = [NSNumber numberWithInt:40];
+        NSNumber *age = [NSNumber numberWithInt:40]; // initialize and pass pointer to 40 to receiver age as NSNumber object
         NSLog(@"I am %@", age);
+        int convertedAge = [age intValue];//convert object vlaue at pointer age to int
+        int birthDayAge = convertedAge+1; //perform numerical functions
+        NSNumber *convertedBirthDayAge = [NSNumber numberWithInt:birthDayAge];//convert back to object
+        NSLog(@"After my birthday, I am %@", convertedBirthDayAge);
     }
     return 0;
 }
